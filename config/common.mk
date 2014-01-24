ff5810dfda5e94a03990d94be7fb9b8f4d21d713
mrimp <mrimp@day-off.net>
2014-01-23 20:52:32 -0500

    Merge CM latest Changes Build changes

--- config/common.mk
+++ config/common.mk
@@ -61,7 +61,7 @@ endif
 
 # Copy over the changelog to the device
 PRODUCT_COPY_FILES += \
-    vendor/cm/CHANGELOG.mkdn:system/etc/CHANGELOG-IOAP.txt
+    vendor/cm/CHANGELOG.mkdn:system/etc/CHANGELOG-CM.txt
 
 # Backup Tool
 ifneq ($(WITH_GMS),true)
