69ae378c8e85151dc0c51dd69b8c6427b5533647
mrimp <mrimp@day-off.net>
2014-03-17 20:34:02 -0400

    rebase

--- config/common_full.mk
+++ config/common_full.mk
@@ -2,10 +2,13 @@
 $(call inherit-product, vendor/cm/config/common.mk)
 
 # Bring in all video files
-# $(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
+$(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
 
 # Include CM audio files
 include vendor/cm/config/cm_audio.mk
+
+# Include CM LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/dictionaries
 
 # Optional CM packages
 PRODUCT_PACKAGES += \
@@ -28,3 +31,7 @@ PRODUCT_PACKAGES += \
     libvideoeditor_osal \
     libvideoeditor_videofilters \
     libvideoeditorplayer
+
+# Extra tools in CM
+PRODUCT_PACKAGES += \
+    vim
